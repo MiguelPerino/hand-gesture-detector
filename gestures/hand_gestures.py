53c0c5bb012417a7d5b90db7586b1fe1b69eef34
MiguelPerino <miguelcperino@gmail.com>
2026-06-08 19:16:25 -0300

    feat: created the function count_fingers, wich count how many fingers are up or down

--- gestures/hand_gestures.py
+++ gestures/hand_gestures.py
@@ -1,6 +1,16 @@
-
+# Indicador 8  6
+# Médio12 10
+# Anelar 16   14
+# Mindinho 20 18
 
 def count_fingers(hand_landmarks):
-    pass
+    fingers_y = [8, 12, 16, 20]
+    middle_y = [6, 10, 14, 18]
 
+    up = []
+    for  tip, joint in zip(fingers_y, middle_y):
+        if hand_landmarks[tip].y < hand_landmarks[joint].y:
+            up.append(1)
     
+    return len(up)
+            
